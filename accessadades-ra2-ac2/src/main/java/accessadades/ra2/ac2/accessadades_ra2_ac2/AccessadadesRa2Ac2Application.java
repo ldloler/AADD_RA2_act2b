4358ad112ef0acfc43ee36dee71cd982904ba768
ldloler <marc.be.pi@gmail.com>
2025-10-22 18:38:49 +0200

    Start

---
+++ accessadades-ra2-ac2/src/main/java/accessadades/ra2/ac2/accessadades_ra2_ac2/AccessadadesRa2Ac2Application.java
@@ -0,0 +1,13 @@
+package accessadades.ra2.ac2.accessadades_ra2_ac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessadadesRa2Ac2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessadadesRa2Ac2Application.class, args);
+	}
+
+}
---
+++ accessadades-ra2-ac2/src/test/java/accessadades/ra2/ac2/accessadades_ra2_ac2/AccessadadesRa2Ac2ApplicationTests.java
@@ -0,0 +1,13 @@
+package accessadades.ra2.ac2.accessadades_ra2_ac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessadadesRa2Ac2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
